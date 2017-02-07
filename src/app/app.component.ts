7f40ca809609f4b50523b63fd786b7e42d706302
Banhawy-Laptop <the.benhawy@gmail.com>
2017-02-06 18:30:37 -0600

    Inject the Hero Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-
-
-const HEROES: Hero [] = [
-	{ id: 11, name: 'Adham'},
-	{ id: 12, name: 'Alan'},
-	{ id: 13, name: 'Jennifer'},
-	{ id: 14, name: 'Sheana'},
-	{ id: 15, name: 'Shane'},
-	{ id: 16, name: 'Luis'},
-	{ id: 17, name: 'Christian'},
-	{ id: 18, name: 'Eddie'},
-	{ id: 19, name: 'Meg'},
-	{ id: 20, name: 'Anna'}
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -92,7 +79,7 @@ export class AppComponent  {
 	  this.selectedHero = hero;
 	}
 
-	heroes = HEROES;
+	heroes: Hero[];
 }
 
 
---
+++ src/app/mock-hero.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero [] = [
+	{ id: 11, name: 'Adham'},
+	{ id: 12, name: 'Alan'},
+	{ id: 13, name: 'Jennifer'},
+	{ id: 14, name: 'Sheana'},
+	{ id: 15, name: 'Shane'},
+	{ id: 16, name: 'Luis'},
+	{ id: 17, name: 'Christian'},
+	{ id: 18, name: 'Eddie'},
+	{ id: 19, name: 'Meg'},
+	{ id: 20, name: 'Anna'}
+];
